50fcc7d4801f4d2745e29f0a78eddc24eaff053f
Lance <lance.auro.c@gmail.com>
2024-05-01 03:34:25 +0800

    video checkpoint 30 11code completion feature

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,11 +1,2 @@
-let sales: number = 123_456_789; // The data type is annotated or hinted.
-let course = 'TypeScript'; // Here, it is implicitly stated.
-let is_published: boolean = true;
-
-let level; // The type is 'any'  for uninitialized variables.
-// level = 1;
-// level = 'a';
-
-function render(document){
-    console.log(document);
-}
+let numbers: number[] = [];
+numbers.forEach(n => n.) // Exhibits code completion upon typing '.'
