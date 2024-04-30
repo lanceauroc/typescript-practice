77ca2043e03407f5e3014b97e6e41c7c2a710981
Lance <lance.auro.c@gmail.com>
2024-05-01 03:58:35 +0800

    declared enum as const to generate a more optimized js code

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,2 +1,4 @@
-let numbers: number[] = [];
-numbers.forEach(n => n.) // Exhibits code completion upon typing '.'
+const enum Size {Small = 1, Medium, Large};
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
