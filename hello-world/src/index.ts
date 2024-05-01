f103afb3fd2e8f132241a172ad51a8d99d52b8eb
Lance <lance.auro.c@gmail.com>
2024-05-02 03:32:19 +0800

    video checkpoint 39 27 return value is undefined

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,4 +1,4 @@
-const enum Size {Small = 1, Medium, Large};
-let mySize: Size = Size.Medium;
-
-console.log(mySize);
+function calculateTax(income: number): number {
+    if (income < 50_000)
+        return income * 1.2;
+} // returns undefined
