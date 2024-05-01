bc83c68549cbcf33896277f8f732506d7436bfd6
Lance <lance.auro.c@gmail.com>
2024-05-02 05:04:35 +0800

    video checkpoint 45 18 start of advanced types

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,10 +1,9 @@
 "use strict";
-function calculateTax(income, taxYear = 2022) {
-    if ((taxYear || 2022) < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-let result = 0;
-result = calculateTax(30000);
-console.log(result);
+let employee = {
+    id: 1,
+    name: 'Lance',
+    retire: (date) => {
+        console.log(date);
+    }
+};
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,8 +1,8 @@
 let employee: {
     readonly id: number,
     name: string,
-    retire: (date: Date) => void // this is the object's signature (what it expects)
-} = { 
+    retire: (date: Date) => void // this is the method's signature (what it expects); return value : void for no return value
+} = {  // this is the start of the return method
     id: 1, 
     name: 'Lance', 
     retire: (date: Date) => {
