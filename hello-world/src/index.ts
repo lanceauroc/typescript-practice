cd43149cf076dd08e0ba8c38365977cf09abd024
Lance <lance.auro.c@gmail.com>
2024-05-02 04:27:38 +0800

    added default parameter value

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,4 +1,4 @@
-function calculateTax(income: number, taxYear?: number): number {
+function calculateTax(income: number, taxYear = 2022): number {
     if ((taxYear || 2022) < 2022)
         return income * 1.2;
     return income * 1.3;
