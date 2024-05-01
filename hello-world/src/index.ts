f2d2fe2e5b14522ae8951311a13330f54a0f7ff2
Lance <lance.auro.c@gmail.com>
2024-05-02 04:43:31 +0800

    video checkpoint 45 30 optional object properties

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,9 +1,4 @@
-function calculateTax(income: number, taxYear = 2022): number {
-    if ((taxYear || 2022) < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-
-let result = 0;
-result = calculateTax(30_000);
-console.log(result);
+let employee: {
+    id: number,
+    name?: string // made optional
+} = { id: 1, name: ''}; // Cannot be set to null or undefined, you may use ''
