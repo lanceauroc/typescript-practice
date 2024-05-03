c36995d1e3dd457c3181625c9eaa8b2154f3da52
Lance <lance.auro.c@gmail.com>
2024-05-04 02:49:37 +0800

    literal types comment

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,7 +1,8 @@
 // Literal (exact, specific value)
 // let quantity:100 | 150 = 150;
 
-type Quantity = 50 | 100;
-let quantity: Quantity = 100;
+type Quantity = 50 | 100; // a new type is defined
+let quantity: Quantity = 100; // initalize the value of quantity
 
+type Metric = 'cm' | 'inch';
 
